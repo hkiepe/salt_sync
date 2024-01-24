fdceac10e79941a6f3eaa4c89f838cd9406e3972
Henrik Kiepe <hkiepe@ssc-services.de>
2024-01-24 08:47:21 +0100

    installer

---
+++ installer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Make sure to fill settings in settings.conf
+################################################
+
+# Read settings from settings.conf
+
+# copy files to destination
+
+# set rights and owner for the script files
+
+# install the gpg key if user wants that
+
+# create a sample project
+
+# copy the gpg key to the project if requested
